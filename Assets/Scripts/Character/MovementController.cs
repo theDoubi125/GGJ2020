75aeb7a5a752304e3dec1338930221745ef89e4a
Julien Revel <j.revel@dowino.com>
2020-01-31 23:34:11 +0100

    Basic scene with simple camera

---
+++ Assets/Scripts/Character/MovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MovementController : MonoBehaviour
+{
+    public Vector2 input;
+    public float movementForce;
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+        rigidbody.AddForce(new Vector3(input.x, 0, input.y) * movementForce);        
+        
+    }
+}
