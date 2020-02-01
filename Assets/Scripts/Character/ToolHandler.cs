bcf2c2b1d4e245e457cde4c9c090e79ea2aa3775
JRevel <julien.revel@telecomnancy.net>
2020-02-01 20:54:36 +0100

    Added tool system

---
+++ Assets/Scripts/Character/ToolHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolHandler : MonoBehaviour
+{
+    private Tool usedTool;
+
+    public void SetTool(Transform prefab)
+    {
+        usedTool = Instantiate(prefab, transform).GetComponent<Tool>();
+    }
+
+    public void DropTool()
+    {
+        Instantiate(usedTool.droppedVersionPrefab, transform.position, transform.rotation);
+        Destroy(usedTool.gameObject);
+    }
+
+    public bool hasTool
+    {
+        get 
+        {
+            return usedTool != null;
+        }
+    }
+
+    public Tool.ToolType usedToolType
+    {
+        get
+        {
+            return usedTool != null ? usedTool.toolType : Tool.ToolType.None;
+        }
+    }
+}
