09b4275efd10a04b3fec2eaef4be4e5b74b023ad
Lucas <tixier.lucas@gmail.com>
2020-02-01 13:08:27 +0100

    Added main game loop

--- Assets/Scripts/Managers/GameManagerScript.cs
+++ Assets/Scripts/Managers/GameManagerScript.cs
@@ -1,39 +1,127 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using DG.Tweening;
-
+using TMPro;
 
 public class GameManagerScript : MonoBehaviour
 {
+    public enum GameState
+    {
+        Waiting,
+        Arriving,
+        Leaving,
+        PitStop
+    }
+
+    public GameState currentState;
+
     [Header("Tweakable values")]
     public float initialWaitTime;
-
+    public bool repairFinish = false;
+    public bool shipIsArrived = false;
 
     [Header("Prefabs and settings")]
     public GameObject prefabSpaceship;
     public Transform shipSpawnPos;
     public Transform shipRepairPos;
 
+    [Header("Events")]
+    public UnityEvent leavingEvent;
 
+    [Header("UI")]
+    public TextMeshProUGUI timerUI;
+
+    private float waitTimer;
+    private float repairTimer;
+    private float leavingTimer;
     private GameObject currentShip;
     private Ship currentShipStat;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         DOTween.Init();
-        StartCoroutine(InitGame());
+
+        //if (leavingEvent == null)
+        //{
+        //    leavingEvent = new UnityEvent();
+        //}
+
+        //leavingEvent.AddListener(LeavingShip);
+
+        waitTimer = 0.0f;
+        currentState = GameState.Waiting;
+        timerUI.text = "PREPARE THE PIT !";
+        //StartCoroutine(InitGame());
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R))
+        switch (currentState)
+        {
+            case GameState.Waiting:
+                if (waitTimer < 5) {
+                    waitTimer += Time.deltaTime;
+                } else {
+                    StartCoroutine(ArrivingShip());
+                    timerUI.text = "BE PREPARED IT'S COMING !";
+                    currentState = GameState.Arriving;
+                }
+                break;
+            case GameState.Arriving:
+                if(shipIsArrived)
+                {
+                    currentState = GameState.PitStop;
+                }
+                break;
+            case GameState.PitStop:
+                if(!repairFinish)
+                {
+                    repairTimer += Time.deltaTime;
+                    timerUI.text = repairTimer.ToString();
+                }
+                else
+                {
+                    StartCoroutine(LeavingShip());
+                    leavingTimer = 0f;
+                    currentState = GameState.Leaving;
+                }
+                break;
+            case GameState.Leaving:
+                if(leavingTimer < 3)
+                {
+                    leavingTimer += Time.deltaTime;
+                }
+                else
+                {
+                    ResetSettings();
+                    waitTimer = 0f;
+                    currentState = GameState.Waiting;
+                }
+                break;
+            default:
+                break;
+        }
+
+   
+        if (Input.GetKeyDown(KeyCode.R))
         {
             Repair();
         }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+        }
+
+
+
+
     }
 
     IEnumerator InitGame()
@@ -50,32 +138,47 @@ public class GameManagerScript : MonoBehaviour
     }
 
 
-    void ArrivingShip()
+    IEnumerator ArrivingShip()
     {
+        repairTimer = 0.0f;
+
         currentShip = Instantiate(prefabSpaceship, shipSpawnPos.position, Quaternion.identity);
         currentShipStat = currentShip.GetComponent<Ship>();
 
         Sequence shipArrivingSeq = DOTween.Sequence();
         shipArrivingSeq.Append(currentShip.transform.DOMoveX(0, 1))
         .Append(currentShip.transform.DOMoveY(1, 1));
+        yield return shipArrivingSeq.WaitForCompletion();
+        shipIsArrived = true;
     }
 
-    void LeavingShip()
+    IEnumerator LeavingShip()
     {
-        Sequence shipArrivingSeq = DOTween.Sequence();
-        shipArrivingSeq.Append(currentShip.transform.DOMoveY(4, 1))
-            .Append(currentShip.transform.DOMoveX(25, 0.3f));
+        //freeze le timer
+        //repairFinish = true;
+
+        Sequence shipLeavingSeq = DOTween.Sequence();
+        shipLeavingSeq.Append(currentShip.transform.DOMoveY(4, 1))
+            .Append(currentShip.transform.DOMoveX(50, 0.3f));
+        yield return shipLeavingSeq.WaitForCompletion();
+
+
     }
 
-    
+
     void Repair()
     {
         currentShipStat.brokenPart--;
 
         if (currentShipStat != null && currentShipStat.brokenPart == 0 )
         {
-            LeavingShip();
+            repairFinish = true;
         }
+
+    }
+
+    void ResetSettings()
+    {
 
     }
 
